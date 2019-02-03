6be89065e4781ee0537a4e675e6b5275720d5430
Carolina Zheng <swirlinq@gmail.com>
2019-02-03 03:47:32 -0500

    Final fixes

--- fair_server/training/views.py
+++ fair_server/training/views.py
@@ -23,6 +23,8 @@ from aif360.metrics import ClassificationMetric
 from aif360.algorithms.preprocessing import Reweighing
 from aif360.metrics import ClassificationMetric
 
+np.random.seed(0)
+
 # Create your views here.
 def play(request):
     empty_context = dict()
@@ -123,7 +125,7 @@ def train(request):
     y_train = dataset_orig_train.labels.ravel()
     w_train = dataset_orig_train.instance_weights.ravel()
 
-    lmod_orig = LogisticRegression()
+    lmod_orig = LogisticRegression(solver='lbfgs')
     lmod_orig.fit(X_train, y_train,
             sample_weight=dataset_orig_train.instance_weights)
     y_train_pred = lmod_orig.predict(X_train)
@@ -177,7 +179,7 @@ def train(request):
                                         disp = False)
 
         if thresh == best_class_thresh:
-            with open('./training/metrics_transf.pkl', 'wb') as f:
+            with open('./training/metrics_orig.pkl', 'wb') as f:
                 pickle.dump(metric_test_bef, f, protocol=pickle.HIGHEST_PROTOCOL)
 
         bal_acc_arr_orig.append(metric_test_bef["Balanced accuracy"])
@@ -188,7 +190,7 @@ def train(request):
     X_train = scale_transf.fit_transform(dataset_transf_train.features)
     y_train = dataset_transf_train.labels.ravel()
 
-    lmod_transf = LogisticRegression()
+    lmod_transf = LogisticRegression(solver='lbfgs')
     lmod_transf.fit(X_train, y_train,
             sample_weight=dataset_transf_train.instance_weights)
     y_train_pred = lmod_transf.predict(X_train)
@@ -212,7 +214,7 @@ def train(request):
                                         disp = False)
 
         if thresh == best_class_thresh:
-            with open('./training/metrics_orig.pkl', 'wb') as f:
+            with open('./training/metrics_transf.pkl', 'wb') as f:
                 pickle.dump(metric_test_aft, f, protocol=pickle.HIGHEST_PROTOCOL)
 
         bal_acc_arr_transf.append(metric_test_aft["Balanced accuracy"])
@@ -225,120 +227,7 @@ def train(request):
         pickle.dump(lmod_transf, f)
 
     return HttpResponse('Model trained')
-=======
-	transf_mean_difference = metric_transf_train.mean_difference()
 
-	with open('./training/transf_mean_difference.pkl', 'wb') as f:
-		pickle.dump(transf_mean_difference, f)
-
-	# Logistic regression classifier and predictions
-	scale_orig = StandardScaler()
-	X_train = scale_orig.fit_transform(dataset_orig_train.features)
-	y_train = dataset_orig_train.labels.ravel()
-	w_train = dataset_orig_train.instance_weights.ravel()
-
-	lmod_orig = LogisticRegression(solver='lbfgs')
-	lmod_orig.fit(X_train, y_train,
-			sample_weight=dataset_orig_train.instance_weights)
-	y_train_pred = lmod_orig.predict(X_train)
-
-	pos_ind = np.where(lmod_orig.classes_ == dataset_orig_train.favorable_label)[0][0]
-
-	dataset_orig_train_pred = dataset_orig_train.copy()
-	dataset_orig_train_pred.labels = y_train_pred
-
-	dataset_orig_valid_pred = dataset_orig_valid.copy(deepcopy=True)
-	X_valid = scale_orig.transform(dataset_orig_valid_pred.features)
-	y_valid = dataset_orig_valid_pred.labels
-	dataset_orig_valid_pred.scores = lmod_orig.predict_proba(X_valid)[:,pos_ind].reshape(-1,1)
-
-	dataset_orig_test_pred = dataset_orig_test.copy(deepcopy=True)
-	X_test = scale_orig.transform(dataset_orig_test_pred.features)
-	y_test = dataset_orig_test_pred.labels
-	dataset_orig_test_pred.scores = lmod_orig.predict_proba(X_test)[:,pos_ind].reshape(-1,1)
-
-	num_thresh = 100
-	ba_arr = np.zeros(num_thresh)
-	class_thresh_arr = np.linspace(0.01, 0.99, num_thresh)
-	for idx, class_thresh in enumerate(class_thresh_arr):
-
-		fav_inds = dataset_orig_valid_pred.scores > class_thresh
-		dataset_orig_valid_pred.labels[fav_inds] = dataset_orig_valid_pred.favorable_label
-		dataset_orig_valid_pred.labels[~fav_inds] = dataset_orig_valid_pred.unfavorable_label
-
-		classified_metric_orig_valid = ClassificationMetric(dataset_orig_valid,
-												dataset_orig_valid_pred,
-												unprivileged_groups=unprivileged_groups,
-												privileged_groups=privileged_groups)
-
-		ba_arr[idx] = 0.5*(classified_metric_orig_valid.true_positive_rate()\
-						+classified_metric_orig_valid.true_negative_rate())
-
-	best_ind = np.where(ba_arr == np.max(ba_arr))[0][0]
-	best_class_thresh = class_thresh_arr[best_ind]
-
-	bal_acc_arr_orig = []
-	disp_imp_arr_orig = []
-	avg_odds_diff_arr_orig = []
-
-	for thresh in tqdm(class_thresh_arr):
-		fav_inds = dataset_orig_test_pred.scores > thresh
-		dataset_orig_test_pred.labels[fav_inds] = dataset_orig_test_pred.favorable_label
-		dataset_orig_test_pred.labels[~fav_inds] = dataset_orig_test_pred.unfavorable_label
-
-		metric_test_bef = compute_metrics(dataset_orig_test, dataset_orig_test_pred,
-										unprivileged_groups, privileged_groups,
-										disp = False)
-
-		if thresh == best_class_thresh:
-			with open('./training/metrics_transf.pkl', 'wb') as f:
-				pickle.dump(metric_test_bef, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-		bal_acc_arr_orig.append(metric_test_bef["Balanced accuracy"])
-		avg_odds_diff_arr_orig.append(metric_test_bef["Average odds difference"])
-		disp_imp_arr_orig.append(metric_test_bef["Disparate impact"])
-
-	scale_transf = StandardScaler()
-	X_train = scale_transf.fit_transform(dataset_transf_train.features)
-	y_train = dataset_transf_train.labels.ravel()
-
-	lmod_transf = LogisticRegression(solver='lbfgs')
-	lmod_transf.fit(X_train, y_train,
-			sample_weight=dataset_transf_train.instance_weights)
-	y_train_pred = lmod_transf.predict(X_train)
-
-	dataset_transf_test_pred = dataset_orig_test.copy(deepcopy=True)
-	X_test = scale_transf.fit_transform(dataset_transf_test_pred.features)
-	y_test = dataset_transf_test_pred.labels
-	dataset_transf_test_pred.scores = lmod_transf.predict_proba(X_test)[:,pos_ind].reshape(-1,1)
-
-	bal_acc_arr_transf = []
-	disp_imp_arr_transf = []
-	avg_odds_diff_arr_transf = []
-
-	for thresh in tqdm(class_thresh_arr):
-		fav_inds = dataset_transf_test_pred.scores > thresh
-		dataset_transf_test_pred.labels[fav_inds] = dataset_transf_test_pred.favorable_label
-		dataset_transf_test_pred.labels[~fav_inds] = dataset_transf_test_pred.unfavorable_label
-
-		metric_test_aft = compute_metrics(dataset_orig_test, dataset_transf_test_pred,
-										unprivileged_groups, privileged_groups,
-										disp = False)
-
-		if thresh == best_class_thresh:
-			with open('./training/metrics_orig.pkl', 'wb') as f:
-				pickle.dump(metric_test_aft, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-		bal_acc_arr_transf.append(metric_test_aft["Balanced accuracy"])
-		avg_odds_diff_arr_transf.append(metric_test_aft["Average odds difference"])
-		disp_imp_arr_transf.append(metric_test_aft["Disparate impact"])
-
-	with open('./training/model_orig.pkl', 'wb') as f:
-		pickle.dump(lmod_orig, f)
-	with open('./training/model_transf.pkl', 'wb') as f:
-		pickle.dump(lmod_transf, f)
-
-	return HttpResponse('Model trained')
 
 def compute_metrics(dataset_true, dataset_pred,
                     unprivileged_groups, privileged_groups,
