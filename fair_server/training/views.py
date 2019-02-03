d0bbce61167052a9e508a485a8a31c99a9071d24
Carolina Zheng <swirlinq@gmail.com>
2019-02-03 03:14:48 -0500

    Add more accuracy metrics

--- fair_server/training/views.py
+++ fair_server/training/views.py
@@ -235,6 +235,9 @@ def compute_metrics(dataset_true, dataset_pred,
                                                  unprivileged_groups=unprivileged_groups,
                                                  privileged_groups=privileged_groups)
     metrics = OrderedDict()
+	metrics["Accuracy (Overall)"] = classified_metric_pred.accuracy()
+	metrics["Accuracy (Privileged)"] = classified_metric_pred.accuracy(True)
+	metrics["Accuracy (Unprivileged)"] = classified_metric_pred.accuracy(False)
     metrics["Balanced accuracy"] = 0.5*(classified_metric_pred.true_positive_rate()+
                                              classified_metric_pred.true_negative_rate())
     metrics["Statistical parity difference"] = classified_metric_pred.statistical_parity_difference()
